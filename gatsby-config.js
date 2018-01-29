5788e98c8fd0a160c10126f73640d7d794328be2
Michael Muré <batolettre@gmail.com>
2018-01-29 21:05:23 +0100

    add the gatsby-plugin-offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Arbore - P2P social file sharing',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline'
+  ],
 };
