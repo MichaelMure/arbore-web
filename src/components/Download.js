5d6c0f66045d9b4ef71a9fe80e83125d9b498833
Michael Muré <batolettre@gmail.com>
2018-02-09 17:07:00 +0100

    replace node.os by a manual detection because linux detection didn't work

--- src/components/Download.js
+++ src/components/Download.js
@@ -2,9 +2,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
-import os from 'node.os'
 import { Button, Container, Grid, Header, Icon, Image, Message, Segment } from "semantic-ui-react"
 import releaseParser from '../models/releaseParser'
+import getOS from '../utils/getos'
 
 export default class Download extends Component {
 
@@ -21,13 +21,13 @@ export default class Download extends Component {
       .then(releases => this.setState({releases}))
   }
 
-  iconOpt = { size: 'big' }
-  voidWindows =  <Icon name='windows' disabled {...this.iconOpt}/>
-  voidMacos = <Icon name='apple' disabled {...this.iconOpt}/>
-  voidLinux = <Icon name='linux' disabled {...this.iconOpt}/>
+  iconOpt     = { size: 'big' }
+  voidLinux   = <Icon name='linux' disabled {...this.iconOpt}/>
+  voidWindows = <Icon name='windows' disabled {...this.iconOpt}/>
+  voidMacos   = <Icon name='apple' disabled {...this.iconOpt}/>
+  linuxLink   = <Link to="/releases"><Icon name='linux' {...this.iconOpt}/></Link>
   windowsLink = (url) => <a href={url}><Icon name='windows' {...this.iconOpt}/></a>
-  macosLink = (url) => <a href={url}><Icon name='apple' {...this.iconOpt}/></a>
-  linuxLink = <Link to="/releases"><Icon name='linux' {...this.iconOpt}/></Link>
+  macosLink   = (url) => <a href={url}><Icon name='apple' {...this.iconOpt}/></a>
 
   description = 'Arbore is a free and open-source file sharing application that enables you to send your pictures, documents, files to your contacts privately and without limits.'
 
@@ -43,25 +43,30 @@ export default class Download extends Component {
       )
     }
 
-    const release = releases.current
+    const current = releases.current
 
-    let button
+    let button, platform
     const text = 'Download Arbore'
     const fallback = <Button as={Link} to='/releases' size='massive'>{text}</Button>
-    switch (os.os) {
-      case 'win':
-        button = release.windowsAssets.length > 0
-          ? <Button as='a' href={release.windowsAssets[0].browserDownloadUrl}>{text}</Button>
+    switch (getOS()) {
+      case 'windows':
+        button = current.windowsAssets.length > 0
+          ? <Button as='a' href={current.windowsAssets[0].browserDownloadUrl}>{text}</Button>
           : fallback
+        platform = 'Windows'
         break
       case 'osx':
-        button = release.macosAssets.length > 0
-          ? <Button as='a' href={release.macosAssets[0].browserDownloadUrl}>{text}</Button>
+        button = current.macosAssets.length > 0
+          ? <Button as='a' href={current.macosAssets[0].browserDownloadUrl}>{text}</Button>
           : fallback
+        platform = 'MacOS'
         break
       case 'linux':
+        button = fallback
+        platform = 'Linux'
+        break
       default:
-        button = fallback;
+        button = fallback
         break
     }
 
@@ -70,17 +75,17 @@ export default class Download extends Component {
         <Header>Arbore</Header>
           { this.description }
           { button }
-
+          Version { current.name }{ platform ? ' - ' + platform : ''  }
           <Container>
-            { release.windowsAssets.length > 0
-              ? this.windowsLink(release.windowsAssets[0].browserDownloadUrl)
+            { current.windowsAssets.length > 0
+              ? this.windowsLink(current.windowsAssets[0].browserDownloadUrl)
               : this.voidWindows
             }
-            { release.macosAssets.length > 0
-              ? this.macosLink(release.macosAssets[0].browserDownloadUrl)
+            { current.macosAssets.length > 0
+              ? this.macosLink(current.macosAssets[0].browserDownloadUrl)
               : this.voidMacos
             }
-            { release.linuxAssets.length > 0
+            { current.linuxAssets.length > 0
               ? this.linuxLink
               : this.voidLinux
             }
@@ -88,7 +93,6 @@ export default class Download extends Component {
       </Container>
     )
   }
-
 }
 
 Download.contextTypes = {
---
+++ src/utils/getos.js
@@ -0,0 +1,19 @@
+// @flow
+export default function getOS() {
+  let OS="windows" //Default
+
+  if (navigator.appVersion.indexOf("Win")!==-1)
+    OS="windows"
+  if (navigator.appVersion.indexOf("Mac")!==-1)
+    OS="osx"
+  if (navigator.platform.indexOf("BeOS")!==-1)
+    OS="beos"
+  if (navigator.platform.indexOf("Linux")!==-1)
+    OS="linux"
+  if (navigator.platform.indexOf("freebsd")!==-1 || navigator.platform.indexOf("FreeBSD")!==-1)
+    OS="freebsd"
+  if (navigator.userAgent.indexOf("iPad")!==-1 || navigator.userAgent.indexOf("iPhone")!==-1 || navigator.userAgent.indexOf("iPod")!==-1)
+    OS="ios"
+
+  return OS
+}
