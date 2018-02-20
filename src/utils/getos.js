82e9a27f27292a16e0d877b1c7ae59bc8aeac67a
Michael Muré <batolettre@gmail.com>
2018-02-20 21:19:10 +0100

    fix android detection

--- src/utils/getos.js
+++ src/utils/getos.js
@@ -8,8 +8,12 @@ export default function getOS() {
     OS="osx"
   if (navigator.platform.indexOf("BeOS")!==-1)
     OS="beos"
-  if (navigator.platform.indexOf("Linux")!==-1)
-    OS="linux"
+  if (navigator.platform.indexOf("Linux")!==-1) {
+    if(navigator.userAgent.indexOf("Android") !== -1)
+      OS="android"
+    else
+      OS="linux"
+  }
   if (navigator.platform.indexOf("freebsd")!==-1 || navigator.platform.indexOf("FreeBSD")!==-1)
     OS="freebsd"
   if (navigator.userAgent.indexOf("iPad")!==-1 || navigator.userAgent.indexOf("iPhone")!==-1 || navigator.userAgent.indexOf("iPod")!==-1)
